d5e38e7f4112d96097925ef2e4dd01ed5b89f95d
vinibilck <bilck.vinicius1998@gmail.com>
2023-04-13 21:31:21 -0300

    PartTypes and Properties in __init__

--- Gahdf5/Gahdf5.py
+++ Gahdf5/Gahdf5.py
@@ -14,6 +14,11 @@ class Gahdf5:
 
     def __init__(self, base_name: str):
         self.base_name = base_name
+        self.parttypes_in_gadget = "gas,halo,disk,bulge,stars,bndry".split(",")
+        self.parttypes_in_hdf5 = "PartType0,PartType1,PartType2,PartType3,PartType4,PartType5".split(",")
+        self.properties_in_gadget = "pos,vel,id,mass,u,rho,hsml,pot,acc".split(",")
+        self.properties_in_hdf5 = "Coordinates,Velocities,ParticleIDs,Masses,InternalEnergy,Density,SmoothingLength," \
+                                  "Potential,Acceleration".split(",")
 
     def create_data(self):
         """
@@ -22,14 +27,12 @@ class Gahdf5:
 
         :return computational: A dictionary with all data
         """
-        parttypes_in_gadget = "gas,halo,disk,bulge,stars,bndry".split(",")
-        properties_in_gadget = "pos,vel,id,mass,u,rho,hsml,pot,acc".split(",")
         uns = uns_in.CUNS_IN(self.base_name, "all", "all", True)
         ok = uns.nextFrame("")
         computational = {}
-        for all_parttypes in parttypes_in_gadget:
+        for all_parttypes in self.parttypes_in_gadget:
             computational[all_parttypes] = {}
-            for all_props in properties_in_gadget:
+            for all_props in self.properties_in_gadget:
                 all_data = uns.getData(all_parttypes, all_props)[1]
                 computational[all_parttypes].update({all_props: all_data})
         return computational
@@ -39,17 +42,12 @@ class Gahdf5:
         Create a hdf5 file format with all the data of
         the base Gadget-2/3 initial condition file
         """
-        parttypes_in_gadget = "gas,halo,disk,bulge,stars,bndry".split(",")
-        parttypes_in_hdf5 = "PartType0,PartType1,PartType2,PartType3,PartType4,PartType5".split(",")
-        properties_in_gadget = "pos,vel,id,mass,u,rho,hsml,pot,acc".split(",")
-        properties_in_hdf5 = "Coordinates,Velocities,ParticleIDs,Masses,InternalEnergy,Density,SmoothingLength," \
-                             "Potential,Acceleration".split(",")
         gadget_file_data = self.create_data()
         with tables.open_file(f"{self.base_name}.hdf5", "w") as hdf5_file:
-            for hdf5_parts, gadget_parts in zip(parttypes_in_hdf5, parttypes_in_gadget):
+            for hdf5_parts, gadget_parts in zip(self.parttypes_in_hdf5, self.parttypes_in_gadget):
                 hdf5_file.create_group("/", hdf5_parts)
                 print(f"Translating Gadget-2/3 {gadget_parts} components to HDF5 {hdf5_parts}")
-                for hdf5_props, gadget_props in zip(properties_in_hdf5, properties_in_gadget):
+                for hdf5_props, gadget_props in zip(self.properties_in_hdf5, self.properties_in_gadget):
                     if (hdf5_props == 'Coordinates') | (hdf5_props == 'Velocities'):
                         old_vector = gadget_file_data[gadget_parts][gadget_props]
                         new_axis = int(len(old_vector) / 3)
